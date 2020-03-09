6993102ef22f64d8155fcca3a52924f0c0deef99
berkansasmaz <berkansasmazz@gmail.com>
2020-03-09 17:32:43 +0300

    Added unit test for the deletePath function.

---
+++ rfsnotify_test.go
@@ -0,0 +1,42 @@
+package rfsnotify
+
+import "testing"
+
+func getSamplesPaths() []string {
+	return []string{"hello", "world", "mars"}
+}
+
+func TestDeletePath_IndexSmallerThanLen_DeletePathAtIndex(t *testing.T) {
+	var index = 1
+	var paths = getSamplesPaths()
+	var result = deletePath(paths, index)
+
+	if len(result) != 2 {
+		t.Fatal("Len(result) should not be bigger than len(paths).")
+	}
+}
+func TestDeletePath_IndexGreaterThanLen_Panics(t *testing.T) {
+	var index = 3
+	var paths = getSamplesPaths()
+	defer func() {
+		if r := recover(); r == nil {
+			t.Fatal("Test must have paniced !")
+		}
+	}()
+	_ = deletePath(paths, index)
+}
+
+func TestDeletePath_IndexIsLenMinusOne_DeletesLastElement(t *testing.T) {
+	var index = 2
+	var paths = getSamplesPaths()
+	var result = deletePath(paths, index)
+
+	if len(result) != 2 {
+		t.Log("Len(result) should have been two.")
+		t.Fail()
+	}
+	if result[1] != "world" {
+		t.Log("result[1] must have been 'world'")
+		t.Fail()
+	}
+}
