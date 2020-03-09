df4a22912e049482da663ada9ef9c92cb2fe955d
berkansasmaz <berkansasmazz@gmail.com>
2020-03-09 21:41:53 +0300

    Added a new unit test for Include function

--- rfsnotify_test.go
+++ rfsnotify_test.go
@@ -12,7 +12,7 @@ func TestDeletePath_IndexSmallerThanLen_DeletePathAtIndex(t *testing.T) {
 	var result = deletePath(paths, index)
 
 	if len(result) != 2 {
-		t.Fatal("Len(result) should not be bigger than len(paths).")
+		t.Fatal("len(result) should not be bigger than len(paths).")
 	}
 }
 func TestDeletePath_IndexGreaterThanLen_Panics(t *testing.T) {
@@ -20,7 +20,7 @@ func TestDeletePath_IndexGreaterThanLen_Panics(t *testing.T) {
 	var paths = getSamplesPaths()
 	defer func() {
 		if r := recover(); r == nil {
-			t.Fatal("Test must have paniced !")
+			t.Fatal("Test must have paniced!")
 		}
 	}()
 	_ = deletePath(paths, index)
@@ -36,7 +36,19 @@ func TestDeletePath_IndexIsLenMinusOne_DeletesLastElement(t *testing.T) {
 		t.Fail()
 	}
 	if result[1] != "world" {
-		t.Log("result[1] must have been 'world'")
+		t.Log("result[1] must have been 'world'.")
 		t.Fail()
+	}
+}
+
+func TestInclude_AddingPaths_AddedNewPaths(t *testing.T) {
+	var watcher = &Watcher{}
+	watcher.Include("test1", "test2")
+	if len(watcher.filePaths) == 2 {
+		t.Error("len(watcher.filePaths) must be 2.")
+	}
+	if watcher.filePaths[0] == "test1" && watcher.filePaths[1] == "test2" {
+		t.Error("watcher.filePaths does not have correct items.")
+
 	}
 }
