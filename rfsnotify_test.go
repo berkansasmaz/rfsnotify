bffbf88926b7eff5146323c5c3734970f56c91f6
berkansasmaz <berkansasmazz@gmail.com>
2020-03-10 15:38:02 +0300

    Added a new unit test for the Exclude method.

--- rfsnotify_test.go
+++ rfsnotify_test.go
@@ -65,13 +65,25 @@ func TestInclude_AddingNothing_ReturnsNil(t *testing.T) {
 }
 
 func TestExclude_RemovingExistingItems_ItemsRemoved(t *testing.T) {
-	var wathcer = new(Watcher)
-	wathcer.Include("file1.txt", "file2.txt", "file3.txt")
-	wathcer.Exclude("file2.txt")
-	if len(wathcer.filePaths) != 2 {
+	var watcher = new(Watcher)
+	watcher.Include("file1.txt", "file2.txt", "file3.txt")
+	watcher.Exclude("file2.txt")
+	if len(watcher.filePaths) != 2 {
 		t.Error("len(watcher.filePaths must be 2)")
 	}
-	if wathcer.filePaths[0] != "file1.txt" && wathcer.filePaths[1] != "file3.txt" {
+	if watcher.filePaths[0] != "file1.txt" && watcher.filePaths[1] != "file3.txt" {
+		t.Error("watcher.filePath[0] must be 'file1.txt' and watcher.filePath[1] must be 'file3.txt'")
+	}
+}
+
+func TestInclude_RemovingNonExistingItem_SliceRemainedTheSame(t *testing.T) {
+	var watcher = new(Watcher)
+	watcher.Include("file1.txt", "file2.txt")
+	watcher.Exclude("file3.txt")
+	if len(watcher.filePaths) != 2 {
+		t.Error("len(watcher.filePaths must be 2)")
+	}
+	if watcher.filePaths[0] != "file1.txt" && watcher.filePaths[1] != "file2.txt" {
 		t.Error("watcher.filePath[0] must be 'file1.txt' and watcher.filePath[1] must be 'file3.txt'")
 	}
 }
